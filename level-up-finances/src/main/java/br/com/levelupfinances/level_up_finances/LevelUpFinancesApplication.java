8bac3e18e1ab715835bf360c499da8f2f4eff8b5
HugoAuroraTech <hugovinidc@gmail.com>
2025-08-22 10:04:26 -0300

    git init

---
+++ level-up-finances/src/main/java/br/com/levelupfinances/level_up_finances/LevelUpFinancesApplication.java
@@ -0,0 +1,13 @@
+package br.com.levelupfinances.level_up_finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpFinancesApplication.class, args);
+	}
+
+}
---
+++ level-up-finances/src/test/java/br/com/levelupfinances/level_up_finances/LevelUpFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.levelupfinances.level_up_finances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
